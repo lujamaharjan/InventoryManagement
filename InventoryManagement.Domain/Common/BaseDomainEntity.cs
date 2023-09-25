c3be6d66fe4e96d5e8b2f70ceb910ec34bb85159
sachin.maharjan <lujamaharjan7@gmail.com>
2023-09-25 20:40:49 +0545

    Add Domain Model

---
+++ InventoryManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Domain.Common
+{
+    public abstract class BaseDomainEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
---
+++ InventoryManagement.Domain/TransactionHistory.cs
@@ -0,0 +1,21 @@
+﻿using InventoryManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Domain
+{
+    /// <summary>
+    ///    Transaction History class is domain model whose main
+    ///    purpose is to track all the items that is gone out and come inside store.
+    /// </summary>
+    public class TransactionHistory:BaseDomainEntity
+    {
+        public Guid ItemId { get; set; }
+        public int Quantity { get; set; }
+        public bool InOut { get; set; }
+        public DateTime OccurredDate { get; set; }
+    }
+}
