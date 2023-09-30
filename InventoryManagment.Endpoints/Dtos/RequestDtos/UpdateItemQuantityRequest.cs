6d55996b4a89cebf87d8d64a875d0e5bc5a58883
sagunrajashakya <76941010+sagunrajashakya@users.noreply.github.com>
2023-09-30 10:35:29 +0545

    Revert "Merge branch 'dev'"
    This reverts commit 8445366f757bc35864a14ef15e2d13512e940ad1, reversing
    changes made to 39f15a0a7f8b23f9f95395d88ccfe03adcb87b73.

--- InventoryManagment.Endpoints/Dtos/RequestDtos/UpdateItemQuantityRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InventoryManagment.Endpoints.Dtos.RequestDtos
-{
-    public class UpdateItemQuantityRequest
-    {
-        public Guid ItemId { get; set; }
-        public int ChangedQuantity { get; set; }
-        public bool InOut { get; set; } 
-    }
-}
--- InventoryManagment.Endpoints/Dtos/ResponseDtos/BaseResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InventoryManagment.Endpoints.Dtos.ResponseDtos
-{
-    public class BaseResponse
-    {
-        public bool Status { get; set; }
-        public string Message { get; set; } = string.Empty;
-        public object? Result { get; set; }
-    }
-}
