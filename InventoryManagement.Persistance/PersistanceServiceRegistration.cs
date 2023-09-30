dd1d0bdb5a0aaaafee07b622549d0f55cd925e7d
sagunrajashakya <76941010+sagunrajashakya@users.noreply.github.com>
2023-09-30 10:38:36 +0545

    Revert "Revert "Merge branch 'dev'""
    This reverts commit 6d55996b4a89cebf87d8d64a875d0e5bc5a58883.

---
+++ InventoryManagement.Persistance/PersistanceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using InventoryManagement.Application.Contracts.Persistance;
+using InventoryManagement.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace InventoryManagement.Persistance
+{
+    public static class PersistanceServiceRegistration
+    {
+        public static IServiceCollection ConfigurePersistanceService(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<InventoryManagementDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("InventoryManagementConnectionString")));
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IItemRepository, ItemRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryItemRepository, CategoryItemRepository>();
+            services.AddScoped<ITransactionHistoryRepository, TransactionHistoryRepository>();
+            return services;
+        }
+
+    }
+}
---
+++ InventoryManagement.Persistance/Repositories/ItemRepository.cs
@@ -0,0 +1,40 @@
+﻿using InventoryManagement.Application.Contracts.Persistance;
+using InventoryManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Persistance.Repositories
+{
+    public class ItemRepository : GenericRepository<Item>, IItemRepository
+    {
+
+        private readonly InventoryManagementDbContext _context;
+        public ItemRepository(InventoryManagementDbContext context):base(context) 
+        {
+            _context = context;
+        }
+
+        public async Task DecreaseItemQuantity(Guid itemId, int quantity)
+        {
+            var item = await Get(itemId);
+            if(item.Quantity < quantity)
+            {
+                throw new Exception("Invalid Quantity");
+            }
+            item.Quantity -= quantity;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task IncreaseItemQuantity(Guid itemId, int quantity)
+        {
+            var item = await Get(itemId);
+            item.Quantity += quantity;
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
